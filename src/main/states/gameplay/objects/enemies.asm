87ab332501b250c57925204500bb06f724511ec0
LaroldsJubilantJunkyard <laroldsjubilantjunkyard@gmail.com>
2023-04-25 22:24:28 -0400

    partial removing macros for enemy. everything ok so far.

--- src/main/states/gameplay/objects/enemies.asm
+++ src/main/states/gameplay/objects/enemies.asm
@@ -86,7 +86,10 @@ UpdateEnemies::
     ld a, 0
     ld [wUpdateEnemiesCounter], a
 
-    CopyAddressToPointerVariable wEnemies, wUpdateEnemiesCurrentEnemyAddress
+    ld a, LOW(wEnemies)
+    ld [wUpdateEnemiesCurrentEnemyAddress+0], a
+    ld a, HIGH(wEnemies)
+    ld [wUpdateEnemiesCurrentEnemyAddress+1], a
 
     jp UpdateEnemies_Loop
 
@@ -104,66 +107,181 @@ UpdateEnemies_NextEnemy:
     ret nc
 
     ; Increase the enemy data our address is pointingtwo
-    IncreasePointerVariableAddress wUpdateEnemiesCurrentEnemyAddress, PER_ENEMY_BYTES_COUNT
+    ld a, [wUpdateEnemiesCurrentEnemyAddress+0]
+    add a, PER_ENEMY_BYTES_COUNT
+    ld  [wUpdateEnemiesCurrentEnemyAddress+0], a
+    ld a, [wUpdateEnemiesCurrentEnemyAddress+1]
+    adc a, 0
+    ld  [wUpdateEnemiesCurrentEnemyAddress+1], a
+
 
 UpdateEnemies_Loop:
 
     ; The first byte is if the current object is active
     ; If it's zero, it's inactive, go to the loop section
-    GetPointerVariableValue wUpdateEnemiesCurrentEnemyAddress, enemy_activeByte, b
-    ld a,b
+    ld a, [wUpdateEnemiesCurrentEnemyAddress+0]
+    ld l, a
+    ld a, [wUpdateEnemiesCurrentEnemyAddress+1]
+    ld h, a
+    ld a, [hli]
     cp 0
     jp z, UpdateEnemies_NextEnemy
 
-
     ; Get our x position
-    GetPointerVariableValue wUpdateEnemiesCurrentEnemyAddress, enemy_xByte, b
-    
+    ld a, [hli]
+    ld b, a
 
     ; get our 16-bit y position
-    GetPointerVariableValue wUpdateEnemiesCurrentEnemyAddress, enemy_yLowByte, c
-    GetPointerVariableValue wUpdateEnemiesCurrentEnemyAddress, enemy_yHighByte, d
+    ld a, [hli]
+    ld c, a
+    ld a, [hli]
+    ld d, a
 
-    push de
-    push bc
+    ; Descale the y psoition
+    srl d
+    rr c
+    srl d
+    rr c
+    srl d
+    rr c
+    srl d
+    rr c
 
 UpdateEnemies_Loop_PlayerCollision:
 
-    DeScale16BitInteger c,d
 
-    Get16BitIntegerNonScaledValue wPlayerPositionX, d
-    Get16BitIntegerNonScaledValue wPlayerPositionY, e
+    ; Get our player's unscaled x position in d
+    ld a, [wPlayerPositionX+0]
+    ld d,a
+
+    ld a, [wPlayerPositionX+1]
+    ld e,a
+
+    srl e
+    rr d
+    srl e
+    rr d
+    srl e
+    rr d
+    srl e
+    rr d
+
+    ; We want to use b real quick
+    ; push onto the stack so we can descale the y position
+    push bc
     
+    ; Get our player's unscaled y position in e
+    ld a, [wPlayerPositionY+0]
+    ld e,a
 
+    ld a, [wPlayerPositionY+1]
+    ld b,a
+
+    srl b
+    rr e
+    srl b
+    rr e
+    srl b
+    rr e
+    srl b
+    rr e
+
+    pop bc
+    push hl
+    push bc
+    push de
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ; Check the x distances. Jump to 'NoCollisionWithPlayer' on failure
-    CheckAbsoluteDifferenceAndJump b,d, 16, NoCollisionWithPlayer
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+    ld a, b
+    ld [wObject1Value], a
+
+    ld a, d
+    ld [wObject2Value], a
+
+    ; Save if the minimum distance
+    ld a, 16
+    ld [wSize], a
+
+    call CheckObjectPositionDifference
+
+    pop de
+    pop bc
+    push bc
+    push de
+
+    ld a, [wResult]
+    cp a, 0
+    jp z, NoCollisionWithPlayer
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ; Check the y distances. Jump to 'NoCollisionWithPlayer' on failure
-    CheckAbsoluteDifferenceAndJump c,e, 16, NoCollisionWithPlayer
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+    ld a, c
+    ld [wObject1Value], a
+
+    ld a, e
+    ld [wObject2Value], a
+
+    ; Save if the minimum distance
+    ld a, 16
+    ld [wSize], a
+
+    call CheckObjectPositionDifference
+
+    ld a, [wResult]
+    cp a, 0
+    jp z, NoCollisionWithPlayer
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     call DamagePlayer
     call DrawLives
 
-    pop bc
     pop de
+    pop bc
+    pop hl
     
     jp UpdateEnemies_DeActivateEnemy
 
 NoCollisionWithPlayer::
 
-    pop bc
     pop de
-
+    pop bc
+    pop hl
 
     ; Get our move speed
-    GetPointerVariableValue wUpdateEnemiesCurrentEnemyAddress, enemy_speedByte, e
+    ld a, [hld]
+    ld e, a
 
-    Increase16BitInteger c,d,e
+    dec hl
     
-    SetPointerVariableValue wUpdateEnemiesCurrentEnemyAddress, enemy_yLowByte,c
-    SetPointerVariableValue wUpdateEnemiesCurrentEnemyAddress, enemy_yHighByte,d
+    ; Get/Set and increase our y low byte
+    ld a, [hl]
+    add a, e
+    ld [hli], a
+    ld c, a
 
-    DeScale16BitInteger c,d
+    ; Get/Set our add remainder for y high byte 
+    ld a, [hl]
+    adc a, 0
+    ld [hl], a
+    ld d, a
+
+    ; Descale our y 
+    srl d
+    rr c
+    srl d
+    rr c
+    srl d
+    rr c
+    srl d
+    rr c
 
     ; See if our non scaled low byte is above 160
     ld a, c
@@ -182,9 +300,17 @@ NoCollisionWithPlayer::
 
 UpdateEnemies_DeActivateEnemy:
 
-    ; if it's y value is grater than 160
+    ; we should be at our speed byte
+    ; decrease four to get to our y low byte
+    dec hl
+    dec hl
+    dec hl
+    dec hl
+
     ; Set as inactive
-    SetPointerVariableValue wUpdateEnemiesCurrentEnemyAddress, enemy_activeByte,0
+    ld a, 0
+    ld [hli], a
+    ld [hli], a
 
     ; Decrease counter
     ld a,[wActiveEnemyCounter]
